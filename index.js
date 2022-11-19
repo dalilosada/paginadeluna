b93ec89d704cba654189b699db5bbca9fe32fcae
dalilosada <dalila.n.losada@gmail.com>
2022-11-19 16:44:41 -0300

    index.html

---
+++ index.js
@@ -0,0 +1,28 @@
+let nombre= "Dali";
+let apellido= "Losada";
+let saludo = ("Hola, mi nombre es " + nombre + " " + apellido)
+
+console.log ("Hola, mi nombre es " + nombre + " " + apellido)
+console.log ("Hola, mi nombre es " + nombre + " " + apellido)
+console.log( saludo )
+let a = 2;
+let b = 3;
+ console.log(a,b)
+
+function saludar(arg1,arg2){
+  console.log ( "soy " + arg1 + " " + arg2)
+}
+
+saludar (nombre,apellido)
+
+nombre + apellido
+
+
+
+
+let nombreIngresado = prompt("Ingrese su nombre");
+
+console.log(nombreIngresado)
+ 
+alert (`¡Hola ${nombreIngresado}!`);
+
